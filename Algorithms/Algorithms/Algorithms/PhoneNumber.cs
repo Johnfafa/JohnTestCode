74f3a5d244a100ed8e5f14e6f0d8c1b014878a91
Johnfafa <596948154@qq.com>
2019-06-04 23:34:41 +0800

    first commit

--- Algorithms/Algorithms/Algorithms/PhoneNumber.cs
+++ Algorithms/Algorithms/Algorithms/PhoneNumber.cs
@@ -40,11 +40,35 @@ namespace Algorithms
             return result;
         }
 
+        public IList<string> Combinations2(string digits)
+        {
+            string[] dict = { null,null,"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+            var result = new List<string>();
+            if (string.IsNullOrEmpty(digits)) return result;
+            var q = new Queue<string>();
+            q.Enqueue(string.Empty);
+            while (q.Count > 0)
+            {
+                var cur = q.Dequeue();
+                if (cur.Length == digits.Length)
+                {
+                    result.Add(cur);
+                }
+                else
+                    foreach (var c in dict[digits[cur.Length]-'0'])
+                    {
+                        q.Enqueue(cur + c);
+                    }
+            }
+
+            return result;
+        }
+
         public void Run()
         {
             var phoneNums = "24585639";
-            var result = Combinations(phoneNums);
-            File.WriteAllLines(@"D:\Study\Timothy\Algorithm\Algorithms\Algorithms\Algorithms\phonenumbers.txt",result);
+            var result = Combinations2(phoneNums);
+            File.WriteAllLines(@"D:\Study\Timothy\Algorithm\Algorithms\Algorithms\Algorithms\phonenumbers2.txt",result);
         }
     }
 }
